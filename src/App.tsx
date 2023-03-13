cd5701c88e58a49a1bc66ee15d8965e774221740
Yuliia Dikun <dikun.yulia@gmail.com>
2023-03-13 21:06:21 +0200

    base: setup for app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
+import { ThemeProvider } from "styled-components";
+import { theme } from 'styles/theme';
+import { GlobalStyleComponent } from 'styles/GlobalStyles';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { lazy } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import SharedLayout from './components/SharedLayout/SharedLayout';
 
+const MainPage = lazy(() => import("./pages/Home/Home"));
+const CoursePage = lazy(() => import("./pages/Course/Course"));
 
 
 function App() {
   return (
-    <div>
-      hello
-      
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+         <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<MainPage />} />            
+            <Route path="course/:id" element={<CoursePage />} />            
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Route>
+        </Routes>
+
+        <ToastContainer
+          autoClose={2000}
+          hideProgressBar={true}
+          position="top-right"
+        />
+        <GlobalStyleComponent />
+     </ThemeProvider>
+    </>
   );
 }
 
---
+++ src/components/SharedLayout/SharedLayout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SharedLayout:React.FC = () => {
+  return (
+    <div>SharedLayout</div>
+  )
+}
+
+export default SharedLayout
