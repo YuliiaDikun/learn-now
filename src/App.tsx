8c4e104ae06b7ae8c29946ff463f4290378c0454
Yuliia Dikun <dikun.yulia@gmail.com>
2023-03-13 20:51:44 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div>
+      hello
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Course/Course.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Course:React.FC = () => {
+  return (
+    <div>Course</div>
+  )
+}
+
+export default Course; 
