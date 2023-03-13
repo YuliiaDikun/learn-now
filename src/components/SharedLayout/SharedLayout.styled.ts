a4fecdd34de571fd08fc9c9789d907fb5e632900
Yuliia Dikun <dikun.yulia@gmail.com>
2023-03-13 21:11:38 +0200

    base: routing

---
+++ src/components/SharedLayout/SharedLayout.styled.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  main {
+    flex-grow: 1;
+    background-color: ${({ theme }) => theme.colors.white};
+  }
+`;
+
+export const Container = styled.div`
+  margin: 0 auto;
+  padding-left: ${({ theme }) => theme.spacing(5)};
+  padding-right: ${({ theme }) => theme.spacing(5)};
+  @media screen and (min-width: ${({ theme }) => theme.media.mobile}) {
+    max-width: ${({ theme }) => theme.media.mobile};
+  }
+  @media screen and (min-width: ${({ theme }) => theme.media.tablet}) {
+    padding-left: ${({ theme }) => theme.spacing(8)};
+    padding-right: ${({ theme }) => theme.spacing(8)};
+    max-width: ${({ theme }) => theme.media.tablet};
+  }
+  @media screen and (min-width: ${({ theme }) => theme.media.desktop}) {
+    padding-left: ${({ theme }) => theme.spacing(4)};
+    padding-right: ${({ theme }) => theme.spacing(4)};
+    max-width: ${({ theme }) => theme.media.desktop};
+  }
+`;
+export const Header = styled.header`
+  width: 100%;
+  transition: all 0.5s;
+  z-index: 9;
+  background-color: ${({ theme }) => theme.colors.dark};
+  color: ${({ theme }) => theme.colors.white};
+`;
--- src/components/SharedLayout/SharedLayout.tsx
+++ src/components/SharedLayout/SharedLayout.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import React, { Suspense} from "react";
+import { Outlet } from "react-router-dom";
+import Loader from "components/Loader/Loader";
+import Navigation from "components/Navigation/Navigation";
+import Footer from "components/Footer/Footer";
+import { Header, Wrapper } from "./SharedLayout.styled";
 const SharedLayout:React.FC = () => {
   return (
-    <div>SharedLayout</div>
-  )
-}
+    <Wrapper>
+      <Header>
+        <Navigation />
+      </Header>
+      <main>
+        <Suspense fallback={<Loader/>}>
+          <Outlet />
+        </Suspense>
+      </main>
+      <Footer />
+    </Wrapper>
+  );
+};
 
-export default SharedLayout
+export default SharedLayout;
